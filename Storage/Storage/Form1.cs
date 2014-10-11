1e095fffea3126fd971fd6f9b1c3678e547506e5
serbii2010 <serbii2010@gmail.com>
2014-10-11 17:26:38 +0700

    storage branch

---
+++ Storage/Storage/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Storage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            WebRequest request = WebRequest.Create(@"http://server.serv/index2.php");
+
+            request.Credentials = CredentialCache.DefaultCredentials;
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Stream dataStream = response.GetResponseStream();
+
+                StreamReader reader = new StreamReader(dataStream);
+
+                string responseFromServer = reader.ReadToEnd();
+
+                textBox1.Text += responseFromServer;
+
+                reader.Close();
+                response.Close();
+
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = ex.ToString();
+
+            }
+        }
+    }
+}
