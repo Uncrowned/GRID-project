bc92d49c2e7ba38f760bbdb87a6a0bee6c2a259b
serbii2010 <serbii2010@gmail.com>
2014-10-16 12:53:10 +0700

    отправка POST на сервер

--- Storage/Storage/Form1.Designer.cs
+++ Storage/Storage/Form1.Designer.cs
@@ -28,18 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(13, 288);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "подключиться";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
@@ -55,7 +45,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(413, 336);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -65,7 +54,6 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Storage/Storage/Form1.cs
+++ Storage/Storage/Form1.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Net;
 using System.Windows.Forms;
+using System.Text;
 
 namespace Storage
 {
@@ -11,31 +12,59 @@ namespace Storage
         {
             InitializeComponent();
 
-            WebRequest request = WebRequest.Create(@"http://server.serv/index2.php");
+            //WebRequest request = WebRequest.Create(@"http://newSite/");
 
-            request.Credentials = CredentialCache.DefaultCredentials;
+            //request.Credentials = CredentialCache.DefaultCredentials;
 
-            try
-            {
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //try
+            //{
+            //    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-                Stream dataStream = response.GetResponseStream();
+            //    Stream dataStream = response.GetResponseStream();
 
-                StreamReader reader = new StreamReader(dataStream);
+            //    StreamReader reader = new StreamReader(dataStream);
 
-                string responseFromServer = reader.ReadToEnd();
+            //    string responseFromServer = reader.ReadToEnd();
 
-                textBox1.Text += responseFromServer;
+            //    textBox1.Text += responseFromServer;
 
-                reader.Close();
-                response.Close();
+            //    reader.Close();
+            //    response.Close();
 
-            }
-            catch (Exception ex)
-            {
-                textBox1.Text = ex.ToString();
+            //}
+            //catch (Exception ex)
+            //{
+            //    textBox1.Text = ex.ToString();
 
-            }
+            //}
+            textBox1.Text = POST("http://newSite/", "a=1&b=2");
+        }
+
+        private string POST(string Url, string Data)
+        {
+          WebRequest req = WebRequest.Create(Url);
+          req.Method = "POST";
+          req.Timeout = 100000;
+          req.ContentType = "application/x-www-form-urlencoded";
+          byte[] sentData = Encoding.GetEncoding(1251).GetBytes(Data);
+          req.ContentLength = sentData.Length;
+          Stream sendStream = req.GetRequestStream();
+          sendStream.Write(sentData, 0, sentData.Length);
+          sendStream.Close();
+          /*WebResponse res = req.GetResponse();
+          Stream ReceiveStream = res.GetResponseStream();
+          StreamReader sr = new StreamReader(ReceiveStream, Encoding.UTF8);
+          //Кодировка указывается в зависимости от кодировки ответа сервера
+          Char[] read = new Char[256];
+          int count = sr.Read(read, 0, 256);
+          string Out = String.Empty;
+          while (count > 0)
+          {
+            String str = new String(read, 0, count);
+            Out += str;
+            count = sr.Read(read, 0, 256);
+          }*/
+          return "";
         }
     }
 }
