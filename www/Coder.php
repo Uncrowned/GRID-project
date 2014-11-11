f16eccafeaaf8c899c3b2232c9134cf288647535
Uncrowned <link_92@list.ru>
2014-11-11 20:41:46 +0700

    coder prototypes

--- www/Coder.php
+++ www/Coder.php
@@ -9,4 +9,16 @@ class Coder {
 		$message = array('answer'=>true);
 		echo json_encode($message);
 	}
+	
+	public function renderError($error) {
+	
+	}
+	
+	public function renderNodeId($id) {
+	
+	}
+	
+	public function renderTask($taskId, $task) {
+	
+	}
 }
